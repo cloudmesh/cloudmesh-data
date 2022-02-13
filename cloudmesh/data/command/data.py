990f691564c9ffdf806f12498230b3899aa4b496
Gregor von Laszewski <laszewski@gmail.com>
2022-02-13 00:13:15 -0500

    trying to fix the argument parsing

--- cloudmesh/data/command/data.py
+++ cloudmesh/data/command/data.py
@@ -15,24 +15,27 @@ class DataCommand(PluginCommand):
         ::
 
           Usage:
-                data compress [--benchmark] [--algorithm=kind --level=n --native --sepopts] FILE [--] LOCATION
-                data uncompress [--benchmark] [--native --sepopts --force] [--] FILE [DESTINATION]
+                data compress [--benchmark] [--algorithm=KIND] [--level=N] [--native] [--sepopts] FILE [--] LOCATION
+                data uncompress [--benchmark] [--native] [--sepopts] [--force] [--] FILE [DESTINATION]
                 data info LOCATION
 
           Compresses the specified item. The default algorithm is xz, Alternative it gz.
 
           Arguments:
-              file   a file or directory name to compress or decompress
-              location  the compression algorithm to use
-
+              FILE         a file or directory name to compress or decompress
+              LOCATION     the compression algorithm to use
+              DESTINATION  the destination where for the uncompression of the directory or file
 
           Options:
-              -h        help
-              --level=n   the level of compression to apply 0 (no compression) to 9 (extreme)
-              --algorithm the algorithm to use; gz, bzip2, xz [default: xz]
-              --native    use the OS provided tar for extraction, otherwise use python [default: True]
-              --sepopts   perform archival and compression as seperate steps [default: False]
-              --force     disables file overwrite protection [default: False].
+              -h                help
+              --level=N         the level of compression to apply 0 (no compression) to 9 (extreme)
+              --algorithm=KIND  the algorithm to use; gz, bzip2, xz [default: xz]
+              --native          use the OS provided tar for extraction, otherwise use python [default: True]
+              --sepopts         perform archival and compression as seperate steps [default: False]
+              --force           disables file overwrite protection [default: False].
+
+          Description:
+            TBD
 
         """
 
